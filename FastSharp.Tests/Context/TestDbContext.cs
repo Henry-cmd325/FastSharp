5a3f0c2bd92e131d7b05552244831f47a56e32da
Henry <henrycanalesv@gmail.com>
2026-04-01 09:19:35 -0600

    tests: Added tests of idSelector functionality

--- FastSharp.Tests/Context/TestDbContext.cs
+++ FastSharp.Tests/Context/TestDbContext.cs
@@ -5,5 +5,6 @@ namespace FastSharp.Tests.Context
     public sealed class TestDbContext(DbContextOptions<TestDbContext> options) : DbContext(options)
     {
         public DbSet<TestModel> Models => Set<TestModel>();
+        public DbSet<TestModelWitoutInterface> ModelsWithoutInterface => Set<TestModelWitoutInterface>();
     }
 }
